dcdfc2d5e7da7a4e1a7ac1decd20e020fdb8d8db
alexskyskysky <lomakin_aleksei@inbox.ru>
2022-01-07 18:49:13 +0300

    n27

--- Статград 17.12.21/n27.py
+++ Статград 17.12.21/n27.py
@@ -2,7 +2,9 @@ with open("D:\\Downloads\\ЕГЭ-22\\Статград 17.21.21\\27-B.txt", "r") 
     N = int(f.readline())
     max_s = 0
     s = 0
-    s30 = [10 ** 10] * 30  # минимальные суммы с разными отстатками k от деления на 30
+    min_s30 = [
+        10 ** 10
+    ] * 30  # минимальные суммы с разными отстатками k от деления на 30
     k = 0
     for i in range(N):
         x = int(f.readline())
@@ -11,9 +13,9 @@ with open("D:\\Downloads\\ЕГЭ-22\\Статград 17.21.21\\27-B.txt", "r") 
             k += 1
         if k % 30 == 0 and s > max_s:
             max_s = s
-        if s30[k % 30] != 10 ** 10:
-            s_k = s - s30[k % 30]
+        if min_s30[k % 30] != 10 ** 10:
+            s_k = s - min_s30[k % 30]
             if s_k > max_s:
                 max_s = s_k
-        s30[k % 30] = min(s, s30[k % 30])
+        min_s30[k % 30] = min(s, min_s30[k % 30])
 print(max_s)
