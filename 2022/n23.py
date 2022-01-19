2882bf0193e1b16f2ff919364597c853ee870ec8
alexskyskysky <lomakin_aleksei@inbox.ru>
2022-01-19 18:04:38 +0300

    n23-24

---
+++ 2022/n23.py
@@ -0,0 +1,15 @@
+def f(a, n):
+    if n == a:
+        return 1
+    if n < a:
+        return 0
+    S = f(a, n - 1)
+    if n % 2 == 0:
+        S += f(a, n // 2)
+    if n % 5 == 0:
+        S += f(a, n // 5)
+    return S
+
+
+R = f(1, 10) * f(10, 20) * f(20, 38)
+print(R)
---
+++ 2022/n24.py
@@ -0,0 +1,9 @@
+f = open("C:\\ЕГЭ-22\\Сброник 12 вариантов\\Вариант 1\\Задание 24\\24.txt", "r")
+t = f.read()
+f.close()
+
+X = [0] * 4
+for i in range(2, len(t)):
+    if t[i - 1] == "A" and t[i] == "D":
+        X[ord(t[i - 2]) - ord("A")] += 1
+print(X)
