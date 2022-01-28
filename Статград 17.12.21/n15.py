7c6a5a54c5fd9588df948f3eb825d083b5d3f9d3
alexskyskysky <lomakin_aleksei@inbox.ru>
2022-01-28 18:05:05 +0300

    fix

--- Статград 17.12.21/n15.py
+++ Статград 17.12.21/n15.py
@@ -1,21 +1,24 @@
 def P(x):
-    return x>19 and x<84
+    return 19 < x <= 84
+
 
 def Q(x):
-    return x>4 and x<51
+    return 4 < x <= 51
 
-def A(a,b,x): # a,b - начало и конец отрезка A, они заранее не известны
-    return x>a and x<b
+
+def A(a, b, x):  # a,b - начало и конец отрезка A, они заранее не известны
+    return a < x <= b
+
 
 min_L = 1000
-for a in range(4,85):
-    for b in range(a+1,85):
+for a in range(4, 85):
+    for b in range(a + 1, 85):
         count_F = 0
         for x in range(100):
-            F = not(Q(x)) or P(x) or A(a,b,x)
-            if F==1:
+            F = not (Q(x)) or P(x) or A(a, b, x)
+            if F == 1:
                 count_F += 1
-        L = b-a
-        if count_F==99 and L<min_L:
+        L = b - a
+        if count_F == 100 and L < min_L:
             min_L = L
 print(min_L)
--- Статград 17.12.21/n24.py
+++ Статград 17.12.21/n24.py
@@ -1,4 +1,4 @@
-f = open('C:\\ЕГЭ-22\\Статград 17.12.21\\24.txt','r')
+f = open("C:\\ЕГЭ-22\\Статград 17.12.21\\24.txt", "r")
 T = f.read()
 f.close()
 
@@ -6,15 +6,13 @@ max_count = 0
 t_count = 0
 count_E = 0
 for i in range(len(T)):
-    if T[i]!='A':
+    if T[i] != "A":
         t_count += 1
-        if T[i]=='E':
+        if T[i] == "E":
             count_E += 1
     else:
         if t_count > max_count and count_E >= 3:
             max_count = t_count
         t_count = 0
         count_E = 0
-print(max_count)
-        
-    
+print(max(t_count, max_count))
