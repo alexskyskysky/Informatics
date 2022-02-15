f54031d5e6cd5ac1ac5a4b23440e7bc591c1837b
alexskyskysky <lomakin_aleksei@inbox.ru>
2022-02-15 09:15:25 +0300

    статград

--- Статград 08.02.22/n14.py
+++ Статград 08.02.22/n14.py
@@ -1,6 +1,7 @@
 N = 5 * 343 ** 8 + 4 * 49 ** 12 + 7 ** 14 - 98
 Counts = [0] * 7
 while N > 0:
-    Counts[N % 7] += 1
+    d = N % 7
+    Counts[d] += 1
     N //= 7
 print(Counts.index(max(Counts)))
--- Статград 08.02.22/n19-21.py
+++ Статград 08.02.22/n19-21.py
@@ -1,38 +1,47 @@
 from functools import lru_cache
 
 
-def NewMoves(Moves, player, move):
-    another_player = 1 if player == 0 else 0
+def NewLastMoves(OldLastMoves, current_player, current_move):
+    another_player = 1 if current_player == 0 else 0
     New = [0, 0]
-    New[player] = move
-    New[another_player] = Moves[another_player]
+    New[current_player] = current_move
+    New[another_player] = OldLastMoves[another_player]
     return tuple(New)
 
 
-def Position(pos):  # S - куча камней, move - последний ход
-    # Moves - список из двух элементов с последним ходом каждого из игроков
-    # player=0 для Пети, player=1 для Вани
-    # move=0 - начало игры, move=1 - до этого было +1, move=2 - до этого было +2, move=3 - до этого было *2
-    S, Moves, player = pos
-    another_player = 1 if player == 0 else 0
+def Position(pos):  # S - куча камней,
+    # LastMoves - список из двух элементов с последним ходом каждого из игроков,
+    # current_player - текущий игрок (=0 для Пети, =1 для Вани),
+    # Moves[]=0 - начало игры, =1 - до этого было +1, =2 - до этого было +2, =3 - до этого было *2
+    S, LastMoves, current_player = pos
+    another_player = 1 if current_player == 0 else 0
 
-    if Moves[player] == 0:
+    if LastMoves[current_player] == 0:
         return (
-            (S + 1, NewMoves(Moves, player, 1), another_player),
-            (S + 2, NewMoves(Moves, player, 2), another_player),
-            (S * 2, NewMoves(Moves, player, 3), another_player),
+            (S + 1, NewLastMoves(LastMoves, current_player, 1), another_player),
+            (S + 2, NewLastMoves(LastMoves, current_player, 2), another_player),
+            (S * 2, NewLastMoves(LastMoves, current_player, 3), another_player),
         )
-    if Moves[player] == 1:
-        return (S + 2, NewMoves(Moves, player, 2), another_player), (S * 2, NewMoves(Moves, player, 3), another_player)
-    if Moves[player] == 2:
-        return (S + 1, NewMoves(Moves, player, 1), another_player), (S * 2, NewMoves(Moves, player, 3), another_player)
-    if Moves[player] == 3:
-        return (S + 1, NewMoves(Moves, player, 1), another_player), (S + 2, NewMoves(Moves, player, 2), another_player)
+    if LastMoves[current_player] == 1:
+        return (
+            (S + 2, NewLastMoves(LastMoves, current_player, 2), another_player),
+            (S * 2, NewLastMoves(LastMoves, current_player, 3), another_player),
+        )
+    if LastMoves[current_player] == 2:
+        return (
+            (S + 1, NewLastMoves(LastMoves, current_player, 1), another_player),
+            (S * 2, NewLastMoves(LastMoves, current_player, 3), another_player),
+        )
+    if LastMoves[current_player] == 3:
+        return (
+            (S + 1, NewLastMoves(LastMoves, current_player, 1), another_player),
+            (S + 2, NewLastMoves(LastMoves, current_player, 2), another_player),
+        )
 
 
 @lru_cache(None)
 def Game(pos):
-    S, Moves, player = pos
+    S = pos[0]
     if S >= 21:
         return "end"
     if any(Game(next_pos) == "end" for next_pos in Position(pos)):
