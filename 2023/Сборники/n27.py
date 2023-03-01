7edf65b057c5012a24c2b28da148ba72ffc63ccc
alexskyskysky <lomakin_aleksei@inbox.ru>
2023-03-01 21:27:20 +0700

    update

--- 2023/Сборники/n27.py
+++ 2023/Сборники/n27.py
@@ -1,7 +1,7 @@
 with open("C:\\ЕГЭ\\ЕГЭ-23\\Сборник 14 вариантов\\Вариант 1\\Задание 27\\27-B.txt") as f:
     n = int(f.readline())
     k = 107
-    max_len = 0
+    min_len = 0
     max_sum = 0
     curr_len = 0
     curr_sum = 0
@@ -14,21 +14,21 @@ with open("C:\\ЕГЭ\\ЕГЭ-23\\Сборник 14 вариантов\\Вари
         if curr_sum % k == 0:
             if curr_sum > max_sum:
                 max_sum = curr_sum
-                max_len = curr_len + 1
+                min_len = curr_len + 1
             elif curr_sum == max_sum:
-                max_len = min(max_len, curr_len + 1)
+                min_len = min(min_len, curr_len + 1)
 
         elif curr_sum - (curr_sum // k) * k in prefix_sum:
             j = prefix_sum[curr_sum - (curr_sum // k) * k]
             if curr_sum - prefix_sum[curr_sum - (curr_sum // k) * k] > max_sum:
                 max_sum = curr_sum - prefix_sum[curr_sum - (curr_sum // k) * k]
-                max_len = curr_len - j
+                min_len = curr_len - j
 
             elif curr_sum - prefix_sum[curr_sum - (curr_sum // k) * k] == max_sum:
-                max_len = min(max_len, curr_len - j)
+                min_len = min(min_len, curr_len - j)
 
         else:
             prefix_sum[curr_sum - (curr_sum // k) * k] = curr_sum
         curr_len += 1
 
-    print(max_len)
+    print(min_len)
